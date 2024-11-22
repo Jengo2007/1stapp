31ab320a02728d768be0f702b082419addea07f6
Jengo2007 <zuyunovmagamed@gmail.com>
2024-11-22 17:04:41 +0300

    Create project

---
+++ bool2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace bool2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            bool result =a%2!=0 && b%2!=0;
+            Console.WriteLine(result);
+        }
+    }
+}
